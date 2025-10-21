321560d8582effd00cf5808e17b6ff68775f7b20
jbanks222 <jalenbanks123@gmail.com>
2025-10-21 15:31:36 -0400

    lab10 updates

--- lab10/lab10_script.js
+++ lab10/lab10_script.js
@@ -40,9 +40,65 @@ usernameInput.addEventListener("input", function(){
         displayusername.style.display = "block"
 
     }
+    else if(usernamevaluelength==20){
+        username_error_msg.textContent="username can't be 20+ characters"
+    }
     else{
         displayusername.style.display = "username must be between 2 and 20 characters"
         btnsubmit.disabled = true
     
+    }
+})
+
+/***
+ * oct 21, 2025
+ * // collect the element
+ * 
+ */
+// collect the element
+const inputpassword = document.querySelector("#password")
+//add a focus event to the input
+inputpassword.addEventListener("focus", function(event){
+    event.classlist.add("active_input_valid")
+})
+// add a blur event to an input
+inputpassword.addEventListener.add("blur", function(){ inputpassword.classlist.remove("active_input_valid")
+    inputpassword.classlist.add("active_input_invalid")
+
+
+})
+
+const originalWord = "javascript"
+ // function to shuffle the original word
+function shuffleword(word){
+    return word.split("")
+    .sort(function(){Math.random()-0.5})
+    .join("")
+}
+
+// show shuffled word 
+const shuffled = shuffleWord(originalWord)
+scrambleword.textContent = shuffled
+
+// collect the elements
+const scramble = document.querySelector("#shuffleword") 
+const guessInput = document.querySelector(".#guessInput")
+const wordFeedback = document.querySelector(`#wordfeedback`)
+
+// add the input event
+guessInput.addEventListener("input", function(){
+    // collect characters typed in the input without begin/end space and 
+    const guees = inputpassword.ariaValueMax.trim().toLowerCase
+
+    if(guess === originalWord){
+        wordFeedback.textContent = "Correct!";
+        wordFeedback.classList.add("active_input_valid0")
+
+    }
+    else{
+        wordFeedback.textContent = "Try again";
+        wordFeedback.textContent = "Correct!";
+        wordFeedback.ClassList.remove("active_input_valid")
+        wordFeedback.ClassList.add("active_input_invalid")
     }
 })
