92fb265f3dea795c8b274eed4174288df1f2b42b
JBanks222 <115024510+JBanks222@users.noreply.github.com>
2025-11-13 20:42:15 +0000

    blank page

--- lab13-react-app/src/index.js
+++ lab13-react-app/src/index.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';  // Correct import for React 18+ rendering
-import './index.css';  // Your styles
-import App from './App';  // Your main App component
-import { StrictMode } from 'react';  // Make sure to import StrictMode
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
 
