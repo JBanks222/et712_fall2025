230950b10fe33bb6334195f391326f48a4a5246a
JBanks222 <115024510+JBanks222@users.noreply.github.com>
2025-11-20 00:42:54 +0000

    lab14 fixed and complete

--- lab14_state/sliceapp/src/App.js
+++ lab14_state/sliceapp/src/App.js
@@ -1,28 +1,61 @@
 import './App.css';
 import { useState } from 'react';
-// const message
+
+// list of three messages
+const message = [
+  "I think, therefore I am",
+  "I have a dream",
+  "The only thing we have to fear is fear itself"
+];
+
 function App() {
-  
-
-  //counter
-  let counter = 1;
-
-  const [counter, setCounter] = useState(1)
-
-  //set inistial state
-  const [isOpen, setIsOpen] = useState(false)
+  // set initial state
+  const [counter, setCounter] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="App">
       <h1 className='title'>Slice card by Jalen Banks</h1>
       <section className='slidewrapper'>
-        <div className='numberwarppaer'>
-          <div><p>1</p> </div>
-          <div><p>2</p> </div>
-          <div><p>3</p> </div>
+        <p className="msg">{message[counter-1]}</p>
+        <div className='numberwrapper'>
+          <div className={counter===1 ? "active" : ""}><p>1</p></div>
+          <div className={counter===2 ? "active" : ""}><p>2</p></div>
+          <div className={counter===3 ? "active" : ""}><p>3</p></div>
         </div>
       </section>
 
+      <div className="btngroup">
+        <button onClick={()=>{
+          if(counter > 1) {
+            setCounter(counter-1);
+          } else {
+            setCounter(message.length);
+          }
+        }}>Previous</button>
+        <button onClick={()=>{
+          if(counter < message.length) {
+            setCounter(counter+1);
+          } else {
+            setCounter(1);
+          }
+        }}>Next</button>
+      </div>
+
+      {/* open and close */}
+      <section className="info">
+        <p>
+          <b>Cat</b> (Felis catus), commonly referred to as domestic cat or house cat
+          <button className="readmorebtn" onClick={()=>setIsOpen(!isOpen)}>{isOpen ? "Hide" : "Read More"}</button>
+        </p>
+      </section>
+      <section>
+        {isOpen && (
+          <div className="readmore">
+            <p>The cat, commonly referred to as the domestic cat or house cat, is a small domesticated carnivorous mammal. It is the only domesticated species of the family Felidae.</p>
+          </div>
+        )}
+      </section>
     </div>
   );
 }
