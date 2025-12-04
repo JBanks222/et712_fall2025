101e3840645595c898853ea252b21fb6b9e7cb14
JBanks222 <115024510+JBanks222@users.noreply.github.com>
2025-12-04 14:54:28 +0000

    lab17

---
+++ lab17/lab17.js
@@ -0,0 +1,14 @@
+console.log("Jalen Banks")
+console.log("-------Example 1: Console-------");
+console.warn("Warning! check the last line of code NOW!")
+console.error("ERROR!, port is not available!")
+console.trace("TRACE LOCATION")
+
+setTimeout(() => {
+    console.warn("Warning! input string for next line");
+}, 3000)
+
+console.log("-------Example 2:creating a symple module file-------");
+const name = require ("./mod.")
+//import helper from "./mod"
+//console.log(name("Jalen"));
